f6c09276973438678b3fbe37abe96b829508c824
Mark <hamilm25@ferris.edu>
2022-07-08 09:45:10 -0400

    "testing"

--- src/CoinDice.java
+++ src/CoinDice.java
@@ -2,29 +2,14 @@
 public class CoinDice {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to the Coin Dice Game!");        //Welcome message
-		int heads = 0;
-		int tails = 0;
-		for(int i = 0; i< 10; i++)  {
+		for(int i = 0; i < 10; i++) {
 			int side = (int)(Math.random() * 2);
 			if(side == 0)  {
 				System.out.println("Heads");
-				heads ++;
 			}
 			else  {
 				System.out.println("Tails");
-				tails++;
 			}
-		}
-		System.out.println("Welcome to the Coin dice game");
-		System.out.println("Number of heads: " + heads);
-		System.out.println("Number of tails: " + tails);
-		
-		for(int i = 0; i< 10; i++)  {
-			int die = (int)(Math.random() * 6) + 1;
-			System.out.println("Dice roll number: " + die);
-			//Add comment to confirm
-			//Test one more after pushing
 		}
 	}
 	
